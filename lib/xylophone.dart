16d24b4866ba6db2e3ef1bd866eaf0168749f193
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-05-31 18:29:02 -0400

    keys play according to which note is pressed

--- lib/xylophone.dart
+++ lib/xylophone.dart
@@ -8,13 +8,38 @@ class Xylophone extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Key(Colors.pink),
-          Key(Colors.purple),
-          Key(Colors.blue),
-          Key(Colors.green),
-          Key(Colors.yellow),
-          Key(Colors.orange),
-          Key(Colors.red),
+          Key(
+            color: Colors.red,
+            note: 'c2',
+          ),
+          Key(
+            color: Colors.orange,
+            note: 'b',
+          ),
+          Key(
+            color: Colors.yellow,
+            note: 'a',
+          ),
+          Key(
+            color: Colors.lightGreen,
+            note: 'g',
+          ),
+          Key(
+            color: Colors.green,
+            note: 'f',
+          ),
+          Key(
+            color: Colors.lightBlue,
+            note: 'e',
+          ),
+          Key(
+            color: Colors.blue,
+            note: 'd',
+          ),
+          Key(
+            color: Colors.purple,
+            note: 'c',
+          ),
         ],
       ),
     );
@@ -23,7 +48,8 @@ class Xylophone extends StatelessWidget {
 
 class Key extends StatelessWidget {
   final Color color;
-  Key(this.color);
+  final String note;
+  Key({this.color, this.note});
 
   AudioPlayer audioPlayer = AudioPlayer();
   static AudioCache player = AudioCache();
@@ -33,9 +59,9 @@ class Key extends StatelessWidget {
     return Expanded(
       child: GestureDetector(
         onTap: () {
-          player.play('sounds/xylophone-b.wav');
+          player.play('sounds/xylophone-$note.wav');
 
-          print('$color tapped');
+          print('$note tapped');
         },
         child: Container(
           color: color,
